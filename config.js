8f0e51ca4402a65213f012f3a4db2fa948c26d6c
Heiko Koelle <acontz.42@gmail.com>
2016-11-24 16:00:43 -0500

    Add config.js and listen on port in server.js

---
+++ config.js
@@ -0,0 +1,47 @@
+// Configuration Array
+var config = {};
+config.web = {};
+config.db = {};
+
+// Web Port
+//
+// This is the port that the web server will listen on.
+config.web.port = 8080;
+
+// Database Host
+//
+// This is the host of the database that will be connected to. The default is
+// localhost.
+config.db.host = '127.0.0.1';
+
+// Database Port
+//
+// This is the port of the database that will be connected to. The default is
+// the default MongoDB port.
+config.db.port = 27017;
+
+// Database Name
+//
+// This is the name of the database that will be connected to. The default is
+// 'cert-manager'.
+config.db.name = 'cert-manager';
+
+// Database Options
+//
+// This is a list of options that will be used when connecting to the database.
+// A full list of options can be found on http://mongoosejs.com/docs/connections.html
+// under the 'Options' header.
+// Example: config.db.options = {user: 'username', pass: 'password'};
+config.db.options = {};
+
+// Database Path
+//
+// This is the compiled path to the database generated from the options above.
+// This usually does not require modification unless you are using advanced options.
+config.db.path = ['mongodb://', config.db.host, ':', config.db.port, '/', config.db.name].join('');
+
+// Export the configuration array
+//
+// This allows other files to access the configuration options listed above. Modifying this
+// may result in unexpected behaviour.
+module.exports = config;
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // Dependencies
-var express = require('express');
+var express = require('express'),
+    config = require('./config');
 
 // Initialize app
 var app = express();
+
+// Listen on specified port
+app.listen(config.web.port, function() {
+  // Log port
+  console.log('Web server listening on port', config.web.port);
+});
